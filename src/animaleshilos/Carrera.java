88c35d7e50d10f32b1450f481cb31346c769db0a
cgonzalez816 <Cgonzalez816@gmail.com>
2016-08-12 14:37:46 -0600

    aun no corre!

--- src/animaleshilos/Carrera.java
+++ src/animaleshilos/Carrera.java
@@ -6,6 +6,10 @@
 package animaleshilos;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -19,7 +23,8 @@ public class Carrera extends JFrame {
     JPanel[] paneles;
     JLabel[] Labels;
     String[] nombres = {"conejo","tortuga","Zorro"};
-    
+    JButton boton;
+    int ancho = 400;
     
     // Constructor
     public Carrera (){
@@ -34,14 +39,29 @@ public class Carrera extends JFrame {
             paneles[n] = new JPanel();
              add(paneles[n]);
              Labels[n] = new JLabel(nombres[n]);
+             Labels[n].setIcon(new ImageIcon(getClass().getResource(nombres[n]+ ".jpg"))); 
              paneles[n].add(Labels[n]);
-             
+             Labels[n].setLocation(0,0);
              
         }
     
+        boton = new JButton("ccomenzar");
+        boton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+           System.out.println("La Carrera Va a Empezar");
+                
+                // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
+        add(boton);
+        
+        
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(200,250);
-        setSize(400,250);
+        setSize(ancho,250);
         setVisible(true);
         
     }
