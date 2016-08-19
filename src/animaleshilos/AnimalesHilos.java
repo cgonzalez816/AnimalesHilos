f990b80f1d671f853123814b04f8a4aa7dbe811b
cgonzalez816 <Cgonzalez816@gmail.com>
2016-08-19 15:36:32 -0600

    guardando los valores , Pend Guardar algo!!

--- src/animaleshilos/AnimalesHilos.java
+++ src/animaleshilos/AnimalesHilos.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package animaleshilos;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 /**
  *
  * @author Cesar Gonzalez Salas
@@ -14,9 +16,28 @@ public class AnimalesHilos {
     /**
      * @param args the command line arguments
      */
-     public static void main(String[] args) {
-     
+     public static void main(String[] args)throws IOException {
+     verProcesos();
      Carrera carrera = new Carrera();
-        
+     
      }
+     
+      private static void verProcesos() throws IOException {
+  // LLAMAMOS LA VARIABLE DE ENTORNO WINDOWS Y EL PROGRAMA Q GESTIONA
+  // LOS PROCESOS
+  String consola = System.getenv("windir")+"\\System32\\"+"tasklist.exe";
+  // Ejecutamos el comando
+  Process proceso=Runtime.getRuntime().exec(consola);
+  //OBTENEMOS EL BUFFER DE SALIDA
+  BufferedReader entrada = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
+  String tmp;
+  while((tmp=entrada.readLine())!=null){
+   System.out.println(tmp);
+   
+  }
+  entrada.close();
+  
+ }
+     
+     
 }
